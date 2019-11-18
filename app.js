0bc66140fe26ef1ec83be8bbb58ea5e911b18d91
Yu Yahagi <yuyahagi2@gmail.com>
2019-11-17 23:22:48 -0800

    Load and display a sprite from a tileset sub-image.

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ function initScreen() {
             "images/obake_kage.png"
         ])
         .load(() => {
-            let obake = new PIXI.Sprite(PIXI.Loader.shared.resources["images/obake.png"].texture);
+            let rectangle = new PIXI.Rectangle(0, 32, 32, 32);
+            let texture = PIXI.utils.TextureCache["images/obake.png"];
+            texture.frame = rectangle;
+            let obake = new PIXI.Sprite(texture);
+            
+            obake.anchor.set(0.5, 0.5);
+            obake.position.set(320, 240);
+            obake.scale.set(2, 2);
+            obake.rotation = 0.1;
+            
             app.stage.addChild(obake);
+            app.renderer.render(app.stage);
         });
 
-    // let mainDiv = document.getElementById('main');
-    // mainDiv.appendChild(app.view);
-    document.body.appendChild(app.view);
+    let mainDiv = document.getElementById('main');
+    mainDiv.appendChild(app.view);
 }
