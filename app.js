93aefa97c4a2ca2ece2e395ea7b5db7a86d3488b
Yu Yahagi <yuyahagi2@gmail.com>
2019-11-18 00:31:23 -0800

    Move a spirite.

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 'use strict'
 
-function initScreen() {
-    let app = new PIXI.Application({ width: 640, height: 480 });
+let app = new PIXI.Application({ width: 640, height: 480 });
+let obake;
 
-    PIXI.Loader.shared
+function initScreen() {
+    app.loader
         .add([
             "images/obake.png",
             "images/obake_kage.png"
         ])
-        .load(() => {
-            let rectangle = new PIXI.Rectangle(0, 32, 32, 32);
-            let texture = PIXI.utils.TextureCache["images/obake.png"];
-            texture.frame = rectangle;
-            let obake = new PIXI.Sprite(texture);
-            
-            obake.anchor.set(0.5, 0.5);
-            obake.position.set(320, 240);
-            obake.scale.set(2, 2);
-            obake.rotation = 0.1;
-            
-            app.stage.addChild(obake);
-            app.renderer.render(app.stage);
-        });
+        .load(setup);
 
     let mainDiv = document.getElementById('main');
     mainDiv.appendChild(app.view);
+}
+
+function setup(loader, resources) {
+    let rectangle = new PIXI.Rectangle(0, 32, 32, 32);
+    let texture = PIXI.utils.TextureCache["images/obake.png"];
+    texture.frame = rectangle;
+    obake = new PIXI.Sprite(texture);
+
+    obake.anchor.set(0.5, 0.5);
+    obake.position.set(320, 240);
+    obake.scale.set(2, 2);
+    obake.rotation = 0.1;
+
+    app.stage.addChild(obake);
+    app.renderer.render(app.stage);
+    
+    app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta) {
+    obake.x += 2 + delta;
 }
