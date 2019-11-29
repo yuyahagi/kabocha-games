ce8f1b2c3df08e3b9a31f370a0119a20481d8f41
Yu Yahagi <yuyahagi2@gmail.com>
2019-11-28 16:50:53 -0800

    Drop incorrect letters.

--- kana.js
+++ kana.js
@@ -9,6 +9,7 @@ let app;
 let input;
 let state = play;
 let letters;
+let fallingLetters = [];
 
 class Letter extends PIXI.Text {
     constructor(text, fill = '#404040', stroke = '#606060') {
@@ -29,14 +30,18 @@ class Letter extends PIXI.Text {
 }
 
 class Word extends PIXI.Container {
-    constructor(letters) {
+    constructor(syllables) {
         super();
-        letters.forEach(c => {
+        syllables.forEach(c => {
             let letter = new Letter(c, '#404040', '#606060');
             letter.x = this.width == 0 ? 0 : this.width + letter.margin;
             this.addChild(letter);
         });
         this.cursor = 0;
+    }
+
+    getLetterAt(idx) {
+        return this.children[idx];
     }
 
     fill(c) {
@@ -48,7 +53,10 @@ class Word extends PIXI.Container {
             this.addChildAt(newLetter, this.cursor);
 
             this.cursor++;
+            return true;
         }
+        else
+            return false;
     }
 
     get allFilled() {
@@ -76,7 +84,7 @@ function setup(loader, resources) {
 }
 
 function initPlay() {
-    letters = new Word(['あ', 'い', 'う', 'ちゃ', 'さ']);// new PIXI.Container();
+    letters = new Word(['も', 'も', 'ちゃ', 'ん']);
     letters.position.set(
         app.screen.width / 2 - letters.width / 2,
         app.screen.height / 2 - letters.height / 2);
@@ -95,28 +103,56 @@ function fillLetter(index, c) {
         app.screen.height / 2 - letters.height / 2);
 }
 
-function addLetter(c) {
-    let newLetter = new Letter(c);
-    newLetter.x = letters.width == 0 ? 0 : letters.width + newLetter.margin;
-    letters.addChild(newLetter);
-
-    letters.position.set(
-        app.screen.width / 2 - letters.width / 2,
-        app.screen.height / 2 - letters.height / 2);
+function moveFallingLetters() {
+    for (var i = fallingLetters.length - 1; i >= 0; i--) {
+        let l = fallingLetters[i];
+        l.x += l.dx;
+        l.y += l.dy;
+        l.dy += 0.2;
+        l.rotation += l.omega;
+        if (l.y > app.screen.height) {
+            fallingLetters.splice(i, 1);
+            app.stage.removeChild(l);
+        }
+    }
 }
 
 function gameLoop(delta) {
     state(delta);
 }
 
-let userInputs = ['あ', 'さ', 'い', 'う', 'う', 'え', 'ちゃ', 'ん'];
+let userInputs = ['も', 'の', 'も', 'ちゃ', 'む', 'ん', 'な'];
 let i = 0;
 
 function play(delta) {
+    let nextInput = userInputs[i];
+
     if (input.pressedZ) {
-        letters.fill(userInputs[i]);
+        const filled = letters.fill(nextInput);
         i = (i + 1) % userInputs.length;
+
+        if (!filled) {
+            const correctLetter = letters.getLetterAt(letters.cursor);
+            let startingPosition = correctLetter.parent.toGlobal(correctLetter.position);
+            startingPosition.x += correctLetter.width / 2;
+            startingPosition.y += correctLetter.height / 2;
+
+            let l = new Letter(nextInput, '#ffffff', '#000000');
+            l.x = startingPosition.x;
+            l.y = startingPosition.y;
+            l.anchor.set(0.5);
+            console.log(startingPosition);
+            console.log(l.position);
+            l.dy = -1;
+            l.dx = 0 * (Math.random() - 0.5);
+            l.omega = 0.05 * (Math.random() - 0.5);
+
+            app.stage.addChild(l);
+            fallingLetters.push(l);
+        }
     }
+
+    moveFallingLetters();
 
     if (letters.allFilled)
         initPlay();
